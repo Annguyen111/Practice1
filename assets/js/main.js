38fe654027152fe651853397567b6008fe97449d
QuocAn11 <anvy1791@gmail.com>
2024-05-04 01:52:20 +0700

    feat: next section

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,40 @@ categorySlider.forEach((item, i) => {
         item.scrollLeft -= containerWidth;
     })
 })
+
+
+// =================================================================
+const featureSlider = [...document.querySelectorAll('.fetured__slider')]
+const nextBtnn = [...document.querySelectorAll('.fetured__btn--next')]
+const prevBtnn = [...document.querySelectorAll('.fetured__btn--prev')]
+
+featureSlider.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nextBtnn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    prevBtnn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
+
+// =================================================================
+const popularSlider = [...document.querySelectorAll('.popular__slider')]
+const nextBtnnn = [...document.querySelectorAll('.popular__btn--next')]
+const prevBtnnn = [...document.querySelectorAll('.popular__btn--prev')]
+
+popularSlider.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nextBtnnn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    prevBtnnn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
